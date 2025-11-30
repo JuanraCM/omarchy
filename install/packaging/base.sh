b10caee8a2e4c75afd72328d563363712a0a221c
JuanraCM <juanracm.dev@gmail.com>
2025-11-30 19:55:30 +0000

    Update packages config

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,3 +1,7 @@
-# Install all base packages
-mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/omarchy-base.packages" | grep -v '^$')
+# Install all base packages (pacman)
+mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/base.packages" | grep -v '^$')
 sudo pacman -S --noconfirm --needed "${packages[@]}"
+
+# Install all AUR packages (yay)
+mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/aur.packages" | grep -v '^$')
+sudo yay -S --noconfirm --needed "${packages[@]}"
